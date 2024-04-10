9fe8d7b1f6004339a51759741b82cb97885b9776
xilconic <xilconic@gmail.com>
2024-04-10 09:24:14 +0200

    Define sprite animations for jumping and falling

--- Mike's Adventure/Assets/Characters/Player/AnimationClipNames.cs
+++ Mike's Adventure/Assets/Characters/Player/AnimationClipNames.cs
@@ -3,4 +3,6 @@
     internal const string Idle = "mike_idle";
     internal const string Walk = "mike_walk";
     internal const string Jog = "mike_jog";
+    internal const string Jump = "mike_jump";
+    internal const string Falling = "mike_falling";
 }
