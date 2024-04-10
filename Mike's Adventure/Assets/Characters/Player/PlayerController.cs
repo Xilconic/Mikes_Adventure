272678964c5db70a41b92da23a4272ab9ea3bd71
xilconic <xilconic@gmail.com>
2024-04-10 11:11:15 +0200

    Implement jump-input buffer + Coyote time
    This makes the experience of 'spamming the jump button' work closed to player intent: jump as quickly as able (even when they were a little too late)

--- Mike's Adventure/Assets/Characters/Player/PlayerController.cs
+++ Mike's Adventure/Assets/Characters/Player/PlayerController.cs
@@ -46,17 +46,32 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Falling gravity scale")]
     public float FallingGravityScale = 2f;
 
+    /// <remarks>
+    /// <para>Average Clicks-per-second for people is about 6.5, which would mean that a jump buffer of ~0.15 on average would leave no gaps when spamming the button.</para>
+    /// <para>UX Research indicated that a <=0.1s response time is typically experienced as 'instantaneous'.</para>
+    /// </remarks>
+    [Tooltip("Jump input buffer, in seconds")]
+    public float JumpBuffer = 0.1f;
+    private float _jumpInputCooldown = 0;
+
+    [Tooltip("Jump input buffer after leaving ground, in seconds")]
+    public float CoyoteTimeBuffer = 0.1f;
+    private float _coyoteTimeCooldown = 0;
+    private bool _touchedGround = false;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _touchingDirections = GetComponent<TouchingDirections>();
+
         Debug.Assert(MaxRunSpeed > 0, "'MaxRunSpeed' must be greater than 0!");
         Debug.Assert(MaxWalkSpeed > 0, "'MaxWalkSpeed' must be greater than 0!");
         Debug.Assert(MaxWalkSpeed <= MaxRunSpeed, "'MaxWalkSpeed' must be less than or equal to 'MaxRunSpeed'!");
         Debug.Assert(JumpImpulse > 0, "'JumpImpulse' must be greater than 0!");
         Debug.Assert(JumpingGravityScale > 0, "'JumpingGravityScale' must be greater than 0!");
         Debug.Assert(FallingGravityScale > 0, "'FallingGravityScale' must be greater than 0!");
+        Debug.Assert(JumpBuffer >= 0, "'JumpBuffer' must be greater than or equal to 0!");
     }
 
     private void Start()
@@ -66,35 +81,67 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+        _jumpInputCooldown = Mathf.Max(0, _jumpInputCooldown - Time.deltaTime);
+        _coyoteTimeCooldown = Mathf.Max(-1, _coyoteTimeCooldown - Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
         _rb.velocity = new Vector2(_movementInput.x * MaxRunSpeed, _rb.velocity.y);
-        if(_touchingDirections.IsGrounded &&
-            !IsRisingInAir) //Not rising into the air
+        if (_touchingDirections.IsGrounded)
         {
-            if (_rb.velocity.x == 0)
+            _touchedGround = true;
+
+            if (_jumpInputCooldown > 0)
             {
-                ChangeAnimationState(AnimationClipNames.Idle);
+                _jumpInputCooldown = 0;
+                ChangeAnimationState(AnimationClipNames.Jump);
+                _rb.gravityScale = JumpingGravityScale;
+                _rb.velocity = new Vector2(_rb.velocity.x, JumpImpulse);
             }
-            else if (Mathf.Abs(_rb.velocity.x) <= MaxWalkSpeed)
+            else if (IsRisingInAir)
             {
-                ChangeAnimationState(AnimationClipNames.Walk);
+                // Keep using the 'Jump' animation clip while going up in the air
+                // TODO: What if we have effects that knock us up in the air? Enemy attacks, traps, bouncy pads? Then we should be setting a 'air rising' animation when not in the middle of the 'jump squat' animation.
             }
             else
             {
-                ChangeAnimationState(AnimationClipNames.Jog);
+                if (_rb.velocity.x == 0)
+                {
+                    ChangeAnimationState(AnimationClipNames.Idle);
+                }
+                else if (Mathf.Abs(_rb.velocity.x) <= MaxWalkSpeed)
+                {
+                    ChangeAnimationState(AnimationClipNames.Walk);
+                }
+                else
+                {
+                    ChangeAnimationState(AnimationClipNames.Jog);
+                }
             }
         }
         else
         {
+            // Went from touching the ground to no longer touching the ground:
+            if (_touchedGround) 
+            {
+                _coyoteTimeCooldown = CoyoteTimeBuffer;
+            }
+            else if (_coyoteTimeCooldown > 0 && _jumpInputCooldown > 0)
+            {
+                _jumpInputCooldown = 0;
+                ChangeAnimationState(AnimationClipNames.Jump);
+                _rb.gravityScale = JumpingGravityScale;
+                _rb.velocity = new Vector2(_rb.velocity.x, JumpImpulse);
+            }
+            
             if (!IsRisingInAir)
             {
                 _rb.gravityScale = FallingGravityScale;
                 ChangeAnimationState(AnimationClipNames.Falling);
             }
+
+            _touchedGround = false;
         }
     }
 
@@ -114,12 +161,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.started &&
-            _touchingDirections.IsGrounded)
+        if(context.started)
         {
-            ChangeAnimationState(AnimationClipNames.Jump);
-            _rb.gravityScale = JumpingGravityScale;
-            _rb.velocity = new Vector2(_rb.velocity.x, JumpImpulse);
+            _jumpInputCooldown = JumpBuffer;
         }
         // Allow for 'short hopping' on release:
         if (context.canceled && IsRisingInAir)
