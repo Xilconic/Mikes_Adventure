242ba7d4850cdbfb4a22590ad6286d3714ee849f
xilconic <xilconic@gmail.com>
2024-04-12 19:21:25 +0200

    Make Mike a audio source for animation behaviors instead

--- Mike's Adventure/Assets/GeneralScripts/Animation/StateMachineBehaviors/PlayAudioClipBehavior.cs
+++ Mike's Adventure/Assets/GeneralScripts/Animation/StateMachineBehaviors/PlayAudioClipBehavior.cs
@@ -8,6 +8,12 @@ public abstract class PlayAudioClipBehavior : StateMachineBehaviour
     [Tooltip("Determines the volume of 'SoundToPlay' when played, in range [0.0, 1.0].")]
     public float Volume = 1f;
 
-    protected void PlaySoundAtAnimator(Animator animator) =>
-        AudioSource.PlayClipAtPoint(SoundToPlay, animator.gameObject.transform.position, Volume);
+    protected void PlaySoundAtAnimator(Animator animator)
+    {
+        var audioSource = animator.GetComponent<AudioSource>();
+        audioSource.clip = SoundToPlay;
+        audioSource.volume = Volume;
+        audioSource.Play();
+        //AudioSource.PlayClipAtPoint(SoundToPlay, animator.gameObject.transform.position, Volume);
+    }
 }
